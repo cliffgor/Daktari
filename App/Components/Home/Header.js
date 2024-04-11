a21270b382569093d1781dabb277a1ef9bd1ee95
cliffgor <clierozigy@gmail.com>
2024-04-11 07:46:52 +0300

    Add header and inside the header add user image and username

---
+++ App/Components/Home/Header.js
@@ -0,0 +1,22 @@
+import { Image, Text, View } from 'react-native'
+
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+
+export default function Header() {
+    const { isLoaded, isSignedIn, user } = useUser()
+    if (!isLoaded || !isSignedIn) {
+        return null
+    }
+    return (
+        <View>
+            <View style={{ display: 'flex', flexDirection: 'row',gap:7,alignItems:'center' }}>
+                <Image source={{ uri: user.imageUrl }} style={{ width: 45, height: 45, borderRadius: 25 }} />
+                <View>
+                    <Text>Welcome,😊</Text>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{user.fullName}</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -1,13 +1,15 @@
 import { Button, Text, View } from 'react-native'
 
+import Header from '../Components/Home/Header';
 import React from 'react'
 import { useAuth } from '@clerk/clerk-expo';
 
 export default function Home() {
     const { isLoaded, signOut } = useAuth();
     return (
-        <View>
-            <Button title="Sign Out" onPress={() => signOut()} />
+        <View style={{padding:20, marginTop:20}}>
+            {/* <Button title="Sign Out" onPress={() => signOut()} /> */}
+            <Header />
             <Text>Home</Text>
         </View>
     )
