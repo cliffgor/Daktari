5923eb4d90813a6340311d79b146d382dacebda8
cliffgor <clierozigy@gmail.com>
2024-04-11 07:25:46 +0300

    FInalize navigation by adding icons

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -1,5 +1,6 @@
 import { Text, View } from 'react-native'
 
+import { AntDesign } from '@expo/vector-icons';
 import Appointment from '../Screens/Appointment'
 import Home from '../Screens/Home.js'
 import Profile from '../Screens/Profile'
@@ -10,10 +11,32 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Appointment" component={Appointment} />
-            <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Navigator screenOptions={
+            {
+                headerShown: false
+            }
+        }>
+            <Tab.Screen name="Home" component={Home}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="home" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen name="Appointment" component={Appointment}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="calendar" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen name="Profile" component={Profile}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="user" size={size} color={color} />
+                    )
+                }}
+            />
         </Tab.Navigator>
     )
 }
